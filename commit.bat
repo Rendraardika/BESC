a583932c9a31b814f039f1130a6aa20c46ef5aac
zaygastore-eng <zaygastore@gmail.com>
2026-08-05 20:26:01 +0700

    cleanup: hapus file temporary deploy

--- commit.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "c:\Users\konta\BESC REAL 3 AGUSTUS"
-git add -A
-git commit -m "fix: tambah field IG/TikTok, fix kelas tidak tersimpan, fix dokumen pendaftaran admin"
-git push origin main
-echo DONE
